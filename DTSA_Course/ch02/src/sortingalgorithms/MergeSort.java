5fa31f7cb83d6588cf2947492ef2f85610adcc9a
star <juniorprogrammer09@gmail.com>
2022-11-10 22:15:32 +0300

    no breaking streak

---
+++ DTSA_Course/ch02/src/sortingalgorithms/MergeSort.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sortingalgorithms;
+
+/**
+ * @author ultimate
+ *
+ */
+public class MergeSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
