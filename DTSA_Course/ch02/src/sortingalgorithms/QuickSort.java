02f4a1fa8e820eb62312f1b2edbe09ec1f1020e7
star <juniorprogrammer09@gmail.com>
2022-11-10 22:15:44 +0300

    no breaking streak

---
+++ DTSA_Course/ch02/src/sortingalgorithms/QuickSort.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sortingalgorithms;
+
+/**
+ * @author ultimate
+ *
+ */
+public class QuickSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
