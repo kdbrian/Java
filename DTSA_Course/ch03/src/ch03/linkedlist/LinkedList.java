d41acf9c2b9427abe0f4320fd031288c8fe7dca8
star <juniorprogrammer09@gmail.com>
2022-11-13 00:34:17 +0300

    added insertSorted and serach methods

--- DTSA_Course/ch03/src/ch03/linkedlist/LinkedList.java
+++ DTSA_Course/ch03/src/ch03/linkedlist/LinkedList.java
@@ -15,7 +15,13 @@ public class LinkedList {
 	 */
 	class Node {
 
+		/***
+		 * The entity to be stored in the list
+		 */
 		private int element;
+		/***
+		 * The link to the next node
+		 */
 		private Node link;
 
 		/**
@@ -59,13 +65,21 @@ public class LinkedList {
 
 	}
 
-	//the head of the list or the first element
+	/***
+	 * the head of the list or the first element
+	 * this is the entry point of the list
+	 */
 	private Node head;
 	
-	//the tail of the list or the last element
+	/***
+	 * the tail of the list or the last element
+	 * this is the exit point of the list
+	 */
 	private Node tail;
 	
-	//keep track of the nodes in the link list
+	/***
+	 * keep track of the number of nodes in the linkedlist
+	 */
 	private int size;
 	
 	/***
@@ -84,7 +98,8 @@ public class LinkedList {
 	public int getSize() { return size; }
 
 	/**
-	 * @param head the head to set
+	 * @param head the {@link #head} to set
+	 * 
 	 */
 	public void setHead(Node head) { this.head = head; }
 
@@ -96,8 +111,8 @@ public class LinkedList {
 	/***
 	 * A method to insert an element into the linked list
 	 * Inserts an element at the end of the list
-	 * sets the element as the head if the list is empty
-	 * the newly inserted element becomes the tail of the linkedlist
+	 * sets the element as the {@link #head} if the list is empty
+	 * the newly inserted element becomes the {@link #tail} of the linkedlist
 	 * @param e he element to be inserted
 	 */
 	public void addLast(int e) {
@@ -114,8 +129,8 @@ public class LinkedList {
 	}
 	
 	/***
-	 * A method to insert an element at the beggining of the link list
-	 * @param e the element to iinsert
+	 * A method to insert an element at the beggining of the linkedlist
+	 * @param e the element to insert
 	 */
 	public void addFirst(int e) {
 		Node newNode = new Node(e, null);
@@ -161,7 +176,7 @@ public class LinkedList {
 	
 	/***
 	 * Function to remove the first element from the list
-	 * @return
+	 * @return value of the removed node or -1 if the list {@link #isEmpty()}
 	 */
 	public int removeFirst() {
 		
@@ -183,6 +198,10 @@ public class LinkedList {
 		
 	}
 	
+	/***
+	 * Method to delete the last element of the linked list or the {@link #tail}
+	 * @return the deleted element  if found and -1 if the list {@link #isEmpty()}
+	 */
 	public int pop() {
 		
 		if(isEmpty()) {
@@ -212,6 +231,98 @@ public class LinkedList {
 	}
 	
 	/***
+	 * A method to delete any element in the linked list provided the position of the element
+	 * @param position
+	 * @return -1 if the position is invalid or list {@link #isEmpty()}
+	 * 			otherwise the deleted element is returne
+	 */
+	public int deleteAny(int position) {
+		
+		if ( isEmpty() || position <= 0 || position> size-1) {
+			System.err.println("Invalid position");
+			return -1;
+		}
+		
+		int i=1;
+		Node currNode = head;
+		
+		
+		while (i< position -1) {
+			currNode=currNode.link;
+			i++;
+		}
+		
+		int el = currNode.link.element;
+		
+		currNode.link = currNode.link.link;
+		size--;
+		return el;
+		
+	}
+	
+	
+	
+	/***
+	 * A method to traverse a linked list and search for a key
+	 * @param key the element to search for
+	 * @return the index of the element if found 
+	 * and -1 otherwise
+	 */
+	public int search(int key) {
+		
+		if (isEmpty())
+			return -1;
+		
+		Node currNode = head;
+		int i=0;//keeps track of the index
+		
+		while (currNode != null && i<size) {
+			
+			if( currNode.element == key)
+				return i;
+			
+			currNode = currNode.link;
+			i++;
+		}
+		return -1;
+	}
+	
+	/***
+	 * Function to insert a node to its sorted position in
+	 * the linkedlist
+	 * @param e the element to be inserted
+	 */
+	public void insertSorted(int e) {
+		
+		Node newNode = new Node(e, null);
+		
+		if(isEmpty())
+			head=newNode;
+		else {
+			
+			Node currNode = head;
+			Node currNode2 = head;
+			
+			while (currNode != null && currNode.element < e) {
+				currNode2 = currNode;
+				currNode = currNode.link;
+			}
+			
+			//if element is smaller than all others
+			if (currNode == head) {
+				newNode.link = head;
+				head = newNode;
+			}else {
+				
+				newNode.link = currNode2.link;
+				currNode2.link = newNode;
+			}
+		}
+		size+=1;
+	}
+	
+	
+	/***
 	 * A method to display the contents of the linked list
 	 */
 	public void display() {
@@ -232,40 +343,40 @@ public class LinkedList {
 		LinkedList list = new LinkedList();
 		
 		// insertion operation
-		list.addLast(10);
-		list.addLast(20);
-		list.addLast(30);
-		list.addLast(33);
+		list.insertSorted(100);
+		list.insertSorted(10);
+		list.insertSorted(20);
+		list.insertSorted(30);
+		list.insertSorted(33);
+		list.insertSorted(32);
+		list.insertSorted(0);
+		list.insertSorted(101);
 		System.out.println("Original list");
 		list.display();
 		
-		list.addFirst(0);
-		System.out.println("After inserting 0 at the beggining");
-		list.display();
+//		list.addFirst(0);
+//		System.out.println("After inserting 0 at the beggining");
+//		list.display();
 		
-		list.addFirst(-1);
-		System.out.println("After inserting -1 at the beggining");
-		list.display();
+//		list.addFirst(-1);
+//		System.out.println("After inserting -1 at the beggining");
+//		list.display();
 		
 		
-		list.insertAt(222, 3);
-		System.out.println("After inserting 222 at the position 3");
-		list.display();
+//		list.insertAt(222, 3);
+//		System.out.println("After inserting 222 at the position 3");
+//		list.display();
 		
-		list.insertAt(90, 5);
-		list.display();
+//		list.insertAt(90, 5);
+//		list.display();
 		
 		// deletion operation
-		System.out.println("Deleted "+list.removeFirst());
-		list.display();
-		
-		System.out.println("Deleted "+list.removeFirst());
-		list.display();
-		
-		System.out.println("Deleted "+list.pop());
-		list.display();
-		
-		System.out.println("Deleted "+list.pop());
-		list.display();
+//		System.out.println("Deleted "+list.removeFirst());
+//		list.display();
+//		System.out.println("Deleted "+list.pop());
+//		list.display();
+//		System.out.println("After deleting : "+list.deleteAny(3));
+//		list.display();
+//		System.out.println("Found 20 at index : "+ list.search(200));
 	}
 }
