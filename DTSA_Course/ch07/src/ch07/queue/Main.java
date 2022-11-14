36463b08884e272cbe6aaf5bc688bf6174b966da
star <juniorprogrammer09@gmail.com>
2022-11-14 21:53:31 +0300

    tested working of QueueArrays

--- DTSA_Course/ch07/src/ch07/queue/Main.java
+++ DTSA_Course/ch07/src/ch07/queue/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		QueueArrays queue = new QueueArrays(5);
+		System.out.println("isEmpty() : "+queue.isEmpty());
+		System.out.println("isFull() : "+queue.isFull());
+		
+		queue.enqueue(10);
+		queue.enqueue(20);
+		queue.enqueue(30);
+		queue.display();
+		System.out.println("Dequeued : "+queue.dequeue());
+		queue.display();
+		System.out.println("isEmpty() : "+queue.isEmpty());
+		System.out.println("isFull() : "+queue.isFull());
+		System.out.println("Length : "+queue.length());
+		System.out.println("Front : "+queue.front());
+		System.out.println("Rear : "+queue.rear());
+		
 	}
 
 }
