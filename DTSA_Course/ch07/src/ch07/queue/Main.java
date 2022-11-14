9e107bed393664e9c0e73d395b5982cc423e9622
star <juniorprogrammer09@gmail.com>
2022-11-14 22:27:20 +0300

    implemented and tested queue linked list ;

--- DTSA_Course/ch07/src/ch07/queue/Main.java
+++ DTSA_Course/ch07/src/ch07/queue/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/*
 		QueueArrays queue = new QueueArrays(5);
 		System.out.println("isEmpty() : "+queue.isEmpty());
 		System.out.println("isFull() : "+queue.isFull());
@@ -20,6 +21,21 @@ public class Main {
 		System.out.println("Length : "+queue.length());
 		System.out.println("Front : "+queue.front());
 		System.out.println("Rear : "+queue.rear());
+		*/
+		
+		/*
+		QueueLinkedList queue = new QueueLinkedList();
+		System.out.println("isEmpty() : "+queue.isEmpty());
+		System.out.println("Length : "+queue.length());
+		queue.enqueue(10);
+		queue.enqueue(20);
+		queue.enqueue(30);
+		queue.display();
+		System.out.println("Dequeued : "+queue.dequeue());
+		queue.display();
+		System.out.println("isEmpty() : "+queue.isEmpty());
+		System.out.println("Length : "+queue.length());
+		*/
 		
 	}
 
