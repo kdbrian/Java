ddd44864357e5326cbda3588d38e42dcb2427007
star <juniorprogrammer09@gmail.com>
2022-11-14 21:45:59 +0300

    implementation using arrays

---
+++ DTSA_Course/ch07/src/ch07/queue/QueueArrays.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package ch07.queue;
+
+/**
+ * @author ultimate
+ * an implementation of the Queue data type using arrays
+ */
+public class QueueArrays {
+
+	// holds the data of the queue
+	private int data[];
+	
+	// holds reference to the front of the queue
+	private int front;
+
+	// holds reference to the back of the queue
+	private int rear;
+	private int size;
+	
+	/***
+	 * creates a new queue
+	 * @param n
+	 */
+	public QueueArrays(int n) {
+		// TODO Auto-generated constructor stub
+		data = new int[n];
+		front=0;
+		rear=0;
+		size=0;
+	}
+	
+	/***
+	 * 
+	 * @return the number of elements in the queue
+	 */
+	public int length() {
+		return size;
+	}
+	
+	/***
+	 * checks if the list is empty
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return size==0;
+	}
+	
+	/***
+	 * checks if the list is full
+	 * @return
+	 */
+	public boolean isFull() {
+		return size == data.length;
+	}
+	
+	/***
+	 * Adds an element to a queue
+	 * @param e the element to be inserted
+	 */
+	public void enqueue(int e) {
+		
+		if(isFull()) {
+			System.err.println("Queue full");
+			return;
+		}
+		
+		data[rear] = e;
+		rear++;
+		size++;
+	}
+	
+	/***
+	 * removes element at the front of the queue and returns it
+	 * @return e the element that has been removed
+	 */
+	public int dequeue() {
+		
+		if(isEmpty()) {
+			System.err.println("Queue empty");
+			return -1;
+		}
+		
+		int el = data[front];
+		
+		front ++; 
+		
+		return el;
+	}
+	
+	public void display() {
+		for (int i = 0; i < rear; i++) {
+			System.out.println(data[i]+"-");
+		}
+		System.out.println();
+	}
+}
