5028f651b4ff8cb36936803c447f0c618e2cc9b0
star <juniorprogrammer09@gmail.com>
2022-11-21 22:41:17 +0300

    tested preorder function

--- DTSA_Course/ch08/src/Main.java
+++ DTSA_Course/ch08/src/Main.java
@@ -1,4 +1,5 @@
 import binarysearchtree.BinarySearchTree;
+import node.Node;
 
 /**
  * 
@@ -17,7 +18,13 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		BinarySearchTree tree = new BinarySearchTree();
-		tree.insert(null, 10);
+		
+		/**
+		 * the root of the tree is set once
+		 * Otherwise we can loose reference to the other nodes
+		 */
+		tree.setRoot(tree.rinsert(null, 10));
+		System.out.println("Inorder");
 		tree.insert(tree.getRoot(), 20);
 		tree.insert(tree.getRoot(), 30);
 		tree.insert(tree.getRoot(), -1);
@@ -25,7 +32,14 @@ public class Main {
 		System.out.println();
 		tree.rinsert(tree.getRoot(), 90);
 		tree.rinsert(tree.getRoot(), 88);
+		tree.insert(tree.getRoot(), -2);
+		tree.insert(tree.getRoot(), 5);
+		tree.insert(tree.getRoot(), 15);
+		tree.insert(tree.getRoot(), 101);
 		tree.inorder(tree.getRoot());
+		System.out.println("\nPreorder");
+		tree.preorder(tree.getRoot());
+		
 	}
 
 }
