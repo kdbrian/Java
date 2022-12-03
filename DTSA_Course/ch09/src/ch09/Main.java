4bb6ce3448cfdfb2150cb139da5c799b23cfeb00
star <juniorprogrammer09@gmail.com>
2022-12-03 23:09:03 +0300

    final testing heap class

--- DTSA_Course/ch09/src/ch09/Main.java
+++ DTSA_Course/ch09/src/ch09/Main.java
@@ -14,11 +14,18 @@ public class Main {
 		heap.display();
 		
 		
+		System.out.println("Size is "+heap.length());
 		heap.insert(10);
 		heap.insert(20);
 		heap.insert(-1);
-		
+		System.out.println("Size is "+heap.length());
 		heap.display();
+		heap.insert(30);
+		heap.insert(-5);
+		heap.insert(14);
+		System.out.println("Size is "+heap.length());
+		heap.display();
+		
 	}
 
 }
