f6f1548ba746590d53ea25090adc9aa9d585e435
star <juniorprogrammer09@gmail.com>
2022-12-03 22:14:46 +0300

    tested the Heap class

--- DTSA_Course/ch09/src/ch09/Main.java
+++ DTSA_Course/ch09/src/ch09/Main.java
@@ -1,10 +1,24 @@
 package ch09;
 
+/***
+ * 
+ * @author ultimate
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Heap heap = new Heap();
+		
+		heap.display();
+		
+		
+		heap.insert(10);
+		heap.insert(20);
+		heap.insert(-1);
+		
+		heap.display();
 	}
 
 }
